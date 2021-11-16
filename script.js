9197ad7fb72e050aa60b8faaf4c3663dc8199c05
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-16 09:38:42 +0700

    add code to make random computer choice from 1 to 10

--- script.js
+++ script.js
@@ -6,4 +6,10 @@ while (repeat) {
   // capture player choice
   alert("Tebak angka dari 1 - 10\nKamu Punya 3 Kesempatan!");
   var player = prompt("Masukkan angka tebakan");
+  // make ramdom computer choice 1 - 10
+  function random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  var computer = random(1, 10);
+  console.log(computer);
 }
