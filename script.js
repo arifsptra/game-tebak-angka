2705240301ed2aa03e58fc8ff352a9809dfdf72f
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-16 12:37:19 +0700

    make a code to determine the rules of the game

--- script.js
+++ script.js
@@ -11,4 +11,27 @@ while (repeat) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   var computer = random(1, 10);
+  // determine the rules
+  // variable to determine the result
+  var result = "";
+  // repetition of answers for players up to 3 times
+  for (live = 2; live > 0; live--) {
+    if (player == computer) {
+      result = "Anda Benar!";
+      // code to display the result
+      alert("Tebakan " + result + "\nAngka yang dicari adalah " + computer);
+    } else if (player < computer) {
+      result = "Anda terlalu RENDAH!";
+      // warns the number of remaining player chances and catches another guess option
+      player = parseInt(prompt("Tebakan " + result + "\nKesempatan Anda Masih " + live + " Lagi"));
+    } else {
+      result = "Anda terlalu TINGGI!";
+      // warns the number of remaining player chances and catches another guess option
+      player = parseInt(prompt("Tebakan " + result + "\nKesempatan Anda Masih " + live + " Lagi"));
+    }
+  }
+  // alert the opportunity is up and the player loses
+  if (live == 0 && player != computer) {
+    alert("Kesempatan Anda sudah habis! \nAnda Kalah!");
+  }
 }
