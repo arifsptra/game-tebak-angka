fcb0e63a661f624002429c59b849582a96ac81f1
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-16 11:19:40 +0700

    change place guess chance variable declaration

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// guess chance variable declaration
-var live = 3;
 // variabel to repeat
 var repeat = true;
 while (repeat) {
+  // guess chance variable declaration
+  var live;
   // capture player choice
   alert("Tebak angka dari 1 - 10\nKamu Punya 3 Kesempatan!");
   var player = parseInt(prompt("Masukkan angka tebakan"));
@@ -11,19 +11,4 @@ while (repeat) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   var computer = random(1, 10);
-  console.log(computer);
-  // determine the rules
-  // variable to determine the result
-  var result = "";
-  for (live; live == 0; live--) {
-    if (player === computer) {
-      result = "Benar!";
-    } else if (player > computer) {
-      result = "Kebanyakan!";
-    } else {
-      result = "Kurang";
-    }
-  }
-  alert(player + " " + computer + " " + result + "\n" + "Live" + live);
-  repeat = confirm();
 }
