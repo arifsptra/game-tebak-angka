6598b184b7bb0f41d2f041847d343e5f803630ba
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-16 10:18:10 +0700

    convert player data type to number

--- script.js
+++ script.js
@@ -5,11 +5,25 @@ var repeat = true;
 while (repeat) {
   // capture player choice
   alert("Tebak angka dari 1 - 10\nKamu Punya 3 Kesempatan!");
-  var player = prompt("Masukkan angka tebakan");
+  var player = parseInt(prompt("Masukkan angka tebakan"));
   // make ramdom computer choice 1 - 10
   function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
   var computer = random(1, 10);
   console.log(computer);
+  // determine the rules
+  // variable to determine the result
+  var result = "";
+  for (live; live == 0; live--) {
+    if (player === computer) {
+      result = "Benar!";
+    } else if (player > computer) {
+      result = "Kebanyakan!";
+    } else {
+      result = "Kurang";
+    }
+  }
+  alert(player + " " + computer + " " + result + "\n" + "Live" + live);
+  repeat = confirm();
 }
