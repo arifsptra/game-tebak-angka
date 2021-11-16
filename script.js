5b88850c47abd056bb130702a9e5413190b8f18d
arifsptra <111202113480@mhs.dinus.ac.id>
2021-11-16 09:32:22 +0700

    add code to capture player choice

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 var live = 3;
 // variabel to repeat
 var repeat = true;
-while (repeat) {}
+while (repeat) {
+  // capture player choice
+  alert("Tebak angka dari 1 - 10\nKamu Punya 3 Kesempatan!");
+  var player = prompt("Masukkan angka tebakan");
+}
